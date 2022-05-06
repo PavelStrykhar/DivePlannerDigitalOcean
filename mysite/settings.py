63755af90315e0f8d8c323ae96eea246377ba3b9
PavelStrykhar <spavlikv@gmail.com>
2022-05-06 14:40:20 +0000

    с дебаг фолс постгрес не рабоает

--- mysite/settings.py
+++ mysite/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')#чуть что удалить, попытка сделать скедитор
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')#чуть что удалить, попытка сделать скедитор
 
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = "/media/"
